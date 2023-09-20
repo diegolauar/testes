b74b0972a69c2caae259100e71f6bf9654505f43
Diego Lauar <diego_pktd@hotmail.com>
2023-09-20 01:12:30 -0300

    Realizando teste de duplicacao ao criar conta

--- src/services/account.js
+++ src/services/account.js
@@ -4,11 +4,15 @@ module.exports = (app) => {
 
     const save = async (account) => {
         if(!account.name) throw new ValidationError('Nome é um atributo obrigatorio')
+
+        const accDb = await find({name: account.name, user_id: account.user_id})
+        if(accDb) throw new ValidationError('Ja existe uma conta com esse nome')
+
         return app.db('accounts').insert(account, '*');
     }
 
-    const findAll = () => {
-        return app.db('accounts')
+    const findAll = (user_id) => {
+        return app.db('accounts').where({user_id})
     }
 
     const find = (filter = {}) => {
--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -4,16 +4,19 @@ const app = require('../../src/app')
 
 const MAIN_ROUTE = ('/v1/accounts')
 let user;
+let user2;
 
-beforeAll(async () => {
+beforeEach(async () => {
     const res = await app.services.user.save({ name: 'User Account', mail: `${Date.now()}@mail.com`, passwd: '1234551' })
     user = { ...res[0] }
     user.token = jwt.encode(user, 'Segredo!')
+    const res2 = await app.services.user.save({ name: 'User Account 2', mail: `${Date.now()}@mail.com`, passwd: '1234551' })
+    user2 = { ...res2[0] }
 })
 
 test('Deve inserir uma conta com sucesso', () => {
     return request(app).post(MAIN_ROUTE)
-        .send({ name: 'Acc #1', user_id: user.id })
+        .send({ name: 'Acc #1' })
         .set('authorization', `bearer ${user.token}`)
         .then((result) => {
             expect(result.status).toBe(201);
@@ -23,7 +26,7 @@ test('Deve inserir uma conta com sucesso', () => {
 
 test('Não deve inserir uma conta sem nome', () => {
     return request(app).post(MAIN_ROUTE)
-        .send({ user_id: user.id })
+        .send({})
         .set('authorization', `bearer ${user.token}`)
         .then((result) => {
             expect(result.status).toBe(400);
@@ -31,22 +34,46 @@ test('Não deve inserir uma conta sem nome', () => {
         })
 })
 
-test('Deve listar todas as contas', () => {
-    return app.db('accounts')
-        .insert({ name: 'Acc list', user_id: user.id })
-        .then(() => request(app).get(MAIN_ROUTE)
-        .set('authorization', `bearer ${user.token}`))
+test('Nao deve inserir uma conta de nome duplciado, para o mesmo usuario', () => {
+    return app.db('accounts').insert({name: 'Acc Duplicada', user_id: user.id})
+    .then(() => request(app).post(MAIN_ROUTE)
+    .set('authorization', `bearer ${user.token}`)
+    .send({name: 'Acc Duplicada'}))
+    .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Ja existe uma conta com esse nome');
+    })
+})
+
+// test('Deve listar todas as contas', () => {
+//     return app.db('accounts')
+//         .insert({ name: 'Acc list', user_id: user.id })
+//         .then(() => request(app).get(MAIN_ROUTE)
+//             .set('authorization', `bearer ${user.token}`))
+//         .then((res) => {
+//             expect(res.status).toBe(200)
+//             expect(res.body.length).toBeGreaterThan(0)
+//         })
+// })
+
+test('Deve listar apenas as contas dos usuario', () => {
+    return app.db('accounts').insert([
+        { name: 'Acc User 1', user_id: user.id },
+        { name: 'Acc User 2', user_id: user2.id }
+    ]).then(() => request(app).get(MAIN_ROUTE)
+        .set('authorization', `bearer ${user.token}`)
         .then((res) => {
             expect(res.status).toBe(200)
-            expect(res.body.length).toBeGreaterThan(0)
-        })
+            expect(res.body.length).toBe(1)
+            expect(res.body[0].name).toBe('Acc User 1')
+        }))
 })
 
 test('Deve retorna uma conta por ID', () => {
     return app.db('accounts')
         .insert({ name: 'Acc By Id', user_id: user.id }, ['id'])
         .then(acc => request(app).get(`${MAIN_ROUTE}/${acc[0].id}`)
-        .set('authorization', `bearer ${user.token}`))
+            .set('authorization', `bearer ${user.token}`))
         .then((res) => {
             expect(res.status).toBe(200)
             expect(res.body.name).toBe('Acc By Id')
@@ -72,7 +99,7 @@ test('Deve remover uma conta', () => {
     return app.db('accounts')
         .insert({ name: 'Acc to remove', user_id: user.id }, ['id'])
         .then(acc => request(app).delete(`${MAIN_ROUTE}/${acc[0].id}`)
-        .set('authorization', `bearer ${user.token}`))
+            .set('authorization', `bearer ${user.token}`))
         .then((res) => {
             expect(res.status).toBe(204);
         })
