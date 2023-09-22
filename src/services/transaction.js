f4e991beb332944f25fbb9451dfbb269139c04cf
Diego Lauar <diego_pktd@hotmail.com>
2023-09-21 23:40:33 -0300

    Deve retornar uma transição por id

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -2,16 +2,24 @@
 module.exports = (app) => {
     const find = (userId, filter = {}) => {
         return app.db('transactions')
-            .join('accounts','accounts.id', 'acc_id')
+            .join('accounts', 'accounts.id', 'acc_id')
             .where(filter)
             .andWhere('accounts.user_id', '=', userId)
             .select()
     }
 
-    const save = (transaction) => {
+    const findOne = (filter = {}) => {
         return app.db('transactions')
-        .insert(transaction, '*')
+            .where(filter)
+            .first()
     }
 
-    return { find, save }
+    const save = (transaction) => {
+        return app.db('transactions')
+            .insert(transaction, '*')
+    }
+
+
+
+    return { find, save, findOne }
 }
