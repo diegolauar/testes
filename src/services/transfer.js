d9345c6970834bc269d823185e377b7f3def0128
Diego Lauar <diego_pktd@hotmail.com>
2023-09-30 20:50:10 -0300

    Teste, salvando transferencia invalida

--- src/services/transfer.js
+++ src/services/transfer.js
@@ -1,3 +1,4 @@
+const ValidationError = require('../erros/ValidationError')
 
 module.exports = (app) => {
 
@@ -8,6 +9,19 @@ module.exports = (app) => {
     }
 
     const save = async (transfer) => {
+
+        if(!transfer.description) throw new ValidationError('Descrição é um atributo obrigatório')
+        if(!transfer.ammount) throw new ValidationError('Valor é um atributo obrigatório')
+        if(!transfer.date) throw new ValidationError('Data é um atributo obrigatório')
+        if(!transfer.acc_ori_id) throw new ValidationError('Conta de origem é um atributo obrigatório')
+        if(!transfer.acc_dest_id) throw new ValidationError('Conta de destino é um atributo obrigatório')
+        if(transfer.acc_ori_id === transfer.acc_dest_id) throw new ValidationError('Não é possivel transferir de uma conta para a mesma')
+
+        const accounts = await app.db('accounts').whereIn('id', [transfer.acc_dest_id, transfer.acc_ori_id])
+        accounts.forEach(acc => {
+            if(acc.user_id != parseInt(transfer.user_id, 10)) throw new ValidationError(`Conta de numero ${acc.id} não pertence ao usuario`) 
+        });  
+
         const result = await app.db('transfers').insert(transfer, '*')
         const transferId = result[0].id
 
