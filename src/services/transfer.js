dc50dd250517e48a2aa35fe3ac0b9b32713ec85c
Diego Lauar <diego_pktd@hotmail.com>
2023-10-04 15:54:10 -0300

    Validando transfer

--- src/services/transfer.js
+++ src/services/transfer.js
@@ -14,8 +14,7 @@ module.exports = (app) => {
             .first()
     }
 
-    const save = async (transfer) => {
-
+    const validate = async (transfer) => {
         if (!transfer.description) throw new ValidationError('Descrição é um atributo obrigatório')
         if (!transfer.ammount) throw new ValidationError('Valor é um atributo obrigatório')
         if (!transfer.date) throw new ValidationError('Data é um atributo obrigatório')
@@ -27,6 +26,11 @@ module.exports = (app) => {
         accounts.forEach(acc => {
             if (acc.user_id != parseInt(transfer.user_id, 10)) throw new ValidationError(`Conta de numero ${acc.id} não pertence ao usuario`)
         });
+    }
+
+    const save = async (transfer) => {
+        await validate(transfer)
+        
 
         const result = await app.db('transfers').insert(transfer, '*')
         const transferId = result[0].id
@@ -42,6 +46,7 @@ module.exports = (app) => {
     }
 
     const update = async (id, transfer) => {
+        await validate(transfer)
 
         const result = await app.db('transfers')
             .where({id})
